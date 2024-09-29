8502053827f4c3de0f74111e25c946a90aca61fa
Waqas Munir <devwaqas232@gmail.com>
2024-09-29 14:34:14 +0500

    Setup ESLint and Prettier

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
       <View className="h-full items-center justify-center bg-white">
-        <Text className="">Open up App.js to start working on your app!</Text>
+        <Text className="">Uber Clone!</Text>
         <StatusBar style="auto" />
       </View>
     </SafeAreaView>
